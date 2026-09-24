d480ed1fa9a35decd93cd851efdec174057b19bd
axavierj <alexanderjimenez29@gmail.com>
2026-09-24 09:10:26 -0400

    wip

--- js/create.js
+++ js/create.js
@@ -90,6 +90,7 @@ addIngredientButton.onclick = (event) => {
 
   const li = document.createElement("li");
   li.dataset.index = index;
+  li.classList.add("grid", "grid-cols-2");
   li.innerHTML = `${ingredient.quantity} ${ingredient.unit} of ${ingredient.name} <button data-index="${index}" class="btn delete-button [ removeIngredient ]">Remove</button>`;
   previewIngredientsList.appendChild(li);
   const removeButtons = li.querySelectorAll(".removeIngredient");
@@ -117,6 +118,7 @@ addInstructions.onclick = () => {
   }
   instructions.push(instruction);
   const li = document.createElement("li");
+  li.classList.add("grid", "grid-cols-2");
   li.innerHTML = `${instruction} <button data-index="${instructions.length - 1}" class="btn delete-button [ removeInstruction ]">Remove</button>`;
   previewInstructionsList.appendChild(li);
   instructionsInput.value = "";
